e164538a177cd45d86d9059d51487fa5cfc6f504
AngiMangy <kickmango16@gmail.com>
2026-04-13 08:48:02 -0400

    added titles to each HTML and made basic routes in main.py

--- main.py
+++ main.py
@@ -13,3 +13,42 @@ app.config["SECRET_KEY"] = "p455k3y"
 con_str = "mysql://root:cset155@localhost/ecomdb"
 engine = create_engine(con_str, echo = True)
 conn = engine.connect()
+
+## App Routes ##
+
+# Route for LogIn #
+
+@app.route("/")
+def login():
+    render_template('index.html')
+
+# Route for Products/Store
+@app.route("/store")
+def login():
+    render_template('store.html')
+
+# Route for myaccount
+@app.route("/myaccount")
+def login():
+    render_template('myaccount.html')
+
+
+# Route for myorders
+@app.route("/myorders")
+def login():
+    render_template('myorders.html')
+
+# Route for cart
+@app.route("/cart")
+def login():
+    render_template('cart.html')
+
+# Route for vendors
+@app.route("/vnedor")
+def login():
+    render_template('vendor.html')
+
+# Route for admin
+@app.route("/admin")
+def login():
+    render_template('admin.html')
